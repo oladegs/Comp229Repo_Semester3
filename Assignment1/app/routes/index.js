70b58581e68dd60ee9f0832b38d52eb1b5af056e
oladegs <144366155+oladegs@users.noreply.github.com>
2023-10-03 10:57:26 -0400

    Downloaded some dependencies , configured all my routes, add more ejs pages , divided partials into 3
    Assignment1 - Update

--- Assignment1/app/routes/index.js
+++ Assignment1/app/routes/index.js
@@ -1,25 +1,42 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express"); // install express
+var router = express.Router(); // Create a new router
 
+// Making a get request ; res.render - render a page and pass an object with a value
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', 
-  { 
-    title: 'Express' 
-  }
-  );
-});
-
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
-});
-
-router.get('/products', function(req, res, next) {
-  res.render('products', 
-  { 
-    product: 'Products',
-    title: 'Phone'
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Home",
   });
 });
+
+/* GET home page. */
+router.get("/home", function (req, res, next) {
+  res.render("index", {
+    title: "Home",
+  });
+});
+
+/* GET About us page */
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About" });
+});
+
+/* GET Products page */
+router.get("/projects", function (req, res, next) {
+  res.render("projects", {
+    title: "Projects",
+  });
+});
+
+/* GET Services page */
+router.get("/services", function (req, res, next) {
+  res.render("services", { title: "Services" });
+});
+
+/* GET Contact page */
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
+});
+
 //
 module.exports = router;
--- Assignment1/config/app.js
+++ Assignment1/config/app.js
@@ -1,42 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// all the piece part that make the whole express generator experience
+// Installed third party packages
+let createError = require("http-errors");
+let express = require("express"); // dl
+let path = require("path");
+let cookieParser = require("cookie-parser"); // dl
+let logger = require("morgan"); // dl
 
-var indexRouter = require('../app/routes/index');
-var usersRouter = require('../app/routes/users');
+// two routes for our main file and user file
+let indexRouter = require("../app/routes/index");
+let usersRouter = require("../app/routes/users");
 
-var app = express();
+// Creates an instance of the express application and stores a reference in the app variable
+let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../app/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "../app/views")); // configure view /view engine to the app ; read more on MVC
+// Module is the way we connect to our logic , controller is logic behind our routes, and view are the end pages
+app.set("view engine", "ejs"); // npm express -e when generating a b=view
 
-app.use(logger('dev'));
-app.use(express.json());
+// Series of activations
+app.use(logger("dev"));
+app.use(express.json()); // data exchange
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "../public"))); // static routes to access public folder for  the client side
+app.use(express.static(path.join(__dirname, "../node_modules")));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.log(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  //in the event of an error we can render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error", { title: "URL/na" });
 });
 
 module.exports = app;
