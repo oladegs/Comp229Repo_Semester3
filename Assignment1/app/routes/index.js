bc864795e4927c4b0393b0d6019c6803eca32ceb
oladegs <144366155+oladegs@users.noreply.github.com>
2023-10-05 13:48:33 -0400

    Updates  mostly my Projects.ejs view template , modified some other necessary things , project is almost complete
    Assignment 1 - Note

--- Assignment1/app/routes/index.js
+++ Assignment1/app/routes/index.js
@@ -5,38 +5,73 @@ var router = express.Router(); // Create a new router
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "Home",
+    title: "Portfolio",
+    name: "Farouk Oladega",
+    field: "Full Stack Developer",
   });
 });
 
 /* GET home page. */
 router.get("/home", function (req, res, next) {
   res.render("index", {
-    title: "Home",
+    title: "Portfolio",
+    name: "Farouk Oladega",
+    field: "Full Stack Developer",
   });
 });
 
-/* GET About us page */
+/* GET About me page */
 router.get("/about", function (req, res, next) {
-  res.render("about", { title: "About" });
+  res.render("about", {
+    title: "About Me",
+    name: "Farouk Oladega",
+    field: "Full Stack Developer",
+    industry: "Web Development",
+    skill: "programming",
+    principles: "hardwork and discipline",
+    hobby1: "basketball",
+    hobby2: "reading books",
+  });
 });
 
-/* GET Products page */
+/* GET Projects page */
 router.get("/projects", function (req, res, next) {
   res.render("projects", {
-    title: "Projects",
+    title: "My Projects",
+    project1: "Guess The Number",
+    project2: "Play Dice",
+    project3: "Real Estate",
   });
 });
 
 /* GET Services page */
 router.get("/services", function (req, res, next) {
-  res.render("services", { title: "Services" });
+  res.render("services", {
+    title: "Services",
+    services1: "Web Development ",
+    services2: "Mobile App Development",
+    services3: "Programming",
+  });
 });
 
 /* GET Contact page */
 router.get("/contact", function (req, res, next) {
-  res.render("contact", { title: "Contact" });
+  res.render("contact", {
+    title: " Contact",
+    address: " 123 Xyz Street",
+    phone: " 416-888-3476",
+    email: " foladega@my.centennialcollege.ca",
+  });
 });
 
+/* GET Submit Form page */
+router.post("/submitForm", function (req, res, next) {
+  res.redirect("/");
+});
+
+/* GET Submit Form page */
+router.post("/submitForm", function (req, res, next) {
+  res.redirect("/home");
+});
 //
 module.exports = router;
